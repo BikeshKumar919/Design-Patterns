efe7dceca33423472911cce00b385748aaa07f52
BikeshKumar919 <97886731+BikeshKumar919@users.noreply.github.com>
2025-08-24 13:44:07 +0530

    Document Facade Design Pattern in MyPlayer.java

--- Structural/Facade/MyPlayer.java
+++ Structural/Facade/MyPlayer.java
@@ -4,3 +4,11 @@ public class MyPlayer {
         facade.playSongAndToPlayList("Khariyat", "playlist1");
     }
 }
+
+/-------------------------------------------------------------------------------------------------------------------------------------------------------------------/
+
+Definition :
+
+“The Facade Design Pattern is a structural pattern that provides a simplified, unified interface to a complex subsystem. It hides the complexities of the system and makes it easier for clients to use.”
+
+👉 In short: it’s like a front desk at a hotel — instead of dealing with housekeeping, kitchen, and reservations separately, you just call the reception (facade).
